2cd2cea7a1b779a6f6821deb3242f92de4958e91
fatimahaider786 <aimanfatima888786@gmail.com>
2026-04-29 17:57:42 +0500

    update mistake

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.get("/products", (req, res) => {
     res.send(Cards);
 });
 const PORT = process.env.PORT || 5050;
-
-app.listen(PORT,"0.0.0.0", () => {
-    console.log("Server is running at http://localhost:5050");
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is live on port ${PORT}`);
 });
