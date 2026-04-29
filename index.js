e0dc97e30514a5b7929230a2c3584b212f657225
fatimahaider786 <aimanfatima888786@gmail.com>
2026-04-29 17:29:32 +0500

    commet to main

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.get("/products", (req, res) => {
     ];
     res.send(Cards);
 });
+const PORT = process.env.PORT || 5050;
 
-app.listen(5050, () => {
+app.listen(PORT,"0.0.0.0", () => {
     console.log("Server is running at http://localhost:5050");
 });
