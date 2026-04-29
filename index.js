bd799ffe24624ce51a0ca46a4e47a5f2e09257b7
fatimahaider786 <aimanfatima888786@gmail.com>
2026-04-29 18:02:21 +0500

    commit

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get("/products", (req, res) => {
     res.send(Cards);
 });
 const PORT = process.env.PORT || 5050;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server is live on port ${PORT}`);
 });
